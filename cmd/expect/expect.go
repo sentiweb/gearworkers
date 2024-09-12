5ffc6347f43299b5bc1b652676779de1e58de32f
Clément Turbelin <clement.turbelin@upmc.fr>
2024-09-12 14:07:57 +0200

    add wait for tester

--- cmd/expect/expect.go
+++ cmd/expect/expect.go
@@ -3,30 +3,31 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	gearman "github.com/mikespook/gearman-go/client"
 	"github.com/sentiweb/gearworkers/pkg/types"
 )
 
-func GearmanResponseHandler(r *gearman.Response) {
-	result, err := r.Result()
-	if err != nil {
-		log.Printf("Error in response %s", err)
-	}
-	log.Println(string(result))
-}
-
 func main() {
 
 	node := flag.String("node", "", "Node")
+	//wait := flag.Bool("wait", false, "Wait for completion")
 
 	var err error
 
 	flag.Parse()
 
 	name := "expect"
+
+	if *node == "" {
+		fmt.Println("Error -node must be provided")
+		flag.Usage()
+		return
+	}
 
 	payload := types.HttpJobPayload{
 		UrlParams:   map[string]string{"node": *node},
@@ -40,10 +41,32 @@ func main() {
 		log.Fatalf("Error launching client %s", err)
 	}
 	var exitCode int = 0
-	_, err = client.DoBg(name, []byte(body), 0)
+	var handle string
+	handle, err = client.DoBg(name, []byte(body), 0)
 	if err != nil {
 		log.Printf("Error during send : %s", err)
 		exitCode = 1
+	}
+	fmt.Println(handle)
+	start := time.Now()
+	maxWait := 20 * time.Second
+	for {
+		status, err := client.Status(handle)
+		if err != nil {
+			fmt.Println("Error", err)
+			break
+		}
+		if status != nil {
+			fmt.Printf("Final Known: %t Running: %t", status.Known, status.Running)
+			fmt.Printf("Completed %d / %d\n", status.Numerator, status.Denominator)
+			if !(status.Running || status.Known) {
+				break
+			}
+		}
+		<-time.After(time.Second)
+		if time.Since(start) > maxWait {
+			break
+		}
 	}
 	os.Exit(exitCode)
 }
